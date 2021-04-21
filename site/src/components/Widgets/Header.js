dcf85fc329d94a0a63274f8dc0710dc613081e3b
Alex Saltstein <asaltste@gmail.com>
2021-04-21 11:36:03 -0400

    changed nav

--- site/src/components/Widgets/Header.js
+++ site/src/components/Widgets/Header.js
@@ -9,17 +9,17 @@ const Header = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'row', margin: 10 }}>
       <img style={{ height: 50, width: 50 }} src={TripleALogo} />
-      <Link to="/Analytics">
+      <Link to="/home">
+        <Button style={{ margin: 5 }}>Home</Button>
+      </Link>
+      <Link to="/analytics">
         <Button style={{ margin: 5 }}>Analytics</Button>
       </Link>
       <Link to="/cards">
         <Button style={{ margin: 5 }}>MemoCards</Button>
       </Link>
-      <Link to="/home">
-        <Button style={{ margin: 5 }}>Home</Button>
-      </Link>
       <Link to="/">
-        <Button style={{ margin: 5 }}>Login</Button>
+        <Button style={{ margin: 5 }}>Logout</Button>
       </Link>
     </div>
   );
